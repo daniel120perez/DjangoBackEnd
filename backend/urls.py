f937e6f40f46c28d415850151b2473e464683b01
daniel120perez <191219@ids.upchiapas.edu.mx>
2020-12-12 09:40:51 -0600

    Update Api url

--- backend/urls.py
+++ backend/urls.py
@@ -14,11 +14,12 @@ router.register('Workers', views.WorkerViewSet)
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    
+
     path('api/v1/', include(router.urls)),
     url(r'api/v1/format/Workers/(?P<pk>[0-9]+)/$', views.WorkerDetail.as_view()),
 
     path('api/v1/auth/', include('rest_auth.urls')),
     path('api/v1/auth/register/',  include('rest_auth.registration.urls')),
     path('api/v1/auth/accounts/', include('allauth.urls')),
+    # api urls fel.simbolo
 ]
